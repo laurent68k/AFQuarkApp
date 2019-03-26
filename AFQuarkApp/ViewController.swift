c755297cd8005a7a1be65d86001b9dc50c68cf58
Laurent Favard <laurent68k@gmail.com>
2019-03-26 23:11:31 +0100

    Test success with cocoapod

---
+++ AFQuarkApp/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  AFQuarkApp
+//
+//  Created by Laurent Favard on 26/03/2019.
+//  Copyright © 2019 Laurent Favard. All rights reserved.
+//
+
+import UIKit
+import AFQuark
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func alertOk(_ sender: Any) {
+        
+        AFAlert.okAlert(self, title: "Quark", message: "Alert box with OK button", completionHandler: {
+            
+            print("On OK confirmed")
+        })
+    }
+    
+    @IBAction func alertOkCancel(_ sender: Any) {
+        
+        AFAlert.okCancelAlert(self, title: "Quark", message: "Alert box with OK and Cancel buttons", okHandler: {
+            
+            print("On OK button")
+            
+        }, cancelHandler: {
+                
+            print("On Cancel Button")
+        })
+    }
+    
+    @IBAction func alertToast(_ sender: Any) {
+        
+        AFAlert.toast(self, title: "Quark", message: "Alert box as toast", delaySeconds: 1.5, completionHandler: {
+            
+            print("Alert toast closed")
+        })
+    }
+    
+    @IBAction func alertMultiple(_ sender: Any) {
+        
+        var actions: [UIAlertAction] = []
+        
+        actions.append( UIAlertAction(title: "Button 1", style: .default, handler: { _ in print("Button 1 pressed")}))
+        actions.append( UIAlertAction(title: "Button 2", style: .default, handler: { _ in print("Button 2 pressed")}))
+        actions.append( UIAlertAction(title: "Button 3", style: .default, handler: { _ in print("Button 3 pressed")}))
+        
+        AFAlert.alert(self, title: "Quark", message: "Alert with multiple buttons", withActions: actions)
+    }
+    
+    @IBAction func alertSheetMultiple(_ sender: Any) {
+
+        var actions: [UIAlertAction] = []
+        
+        actions.append( UIAlertAction(title: "Button 1", style: .default, handler: { _ in print("Button 1 pressed")}))
+        actions.append( UIAlertAction(title: "Button 2", style: .default, handler: { _ in print("Button 2 pressed")}))
+        actions.append( UIAlertAction(title: "Button 3", style: .default, handler: { _ in print("Button 3 pressed")}))
+        
+        AFAlert.alertSheet(self, title: "Quark", message: "Alert Sheet with multiple buttons", forButton: sender, withActions: actions)
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'AFQuarkApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AFQuarkApp
+  pod 'AFQuark', :git => 'https://github.com/laurent68k/AFQuark.git'
+
+end
